3621379d8e53b4d070000d078ca55da2965600dd
albert bou <albertbou92@gmail.com>
2023-12-20 16:39:32 +0100

    rl envs tests

--- acegen/__init__.py
+++ acegen/__init__.py
@@ -1,6 +1,6 @@
 # RL Environments
-from .smiles_environments.single_step_smiles_env import SingleStepDeNovoEnv
-from .smiles_environments.multi_step_smiles_env import MultiStepDeNovoEnv
+from .smiles_environments.single_step_smiles_env import SingleStepSMILESEnv
+from .smiles_environments.multi_step_smiles_env import MultiStepSMILESEnv
 
 # Vocabulary
 from .vocabulary.vocabulary import SMILESVocabulary
---
+++ acegen/smiles_environments/__init__.py
@@ -0,0 +1,2 @@
+from acegen.smiles_environments.multi_step_smiles_env import MultiStepSMILESEnv
+from acegen.smiles_environments.single_step_smiles_env import SingleStepSMILESEnv
