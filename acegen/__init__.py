b2fbec42666672eb9c8f73b69e04ecebf1d8630a
albert bou <albertbou92@gmail.com>
2024-01-04 17:33:16 +0100

    fix sac

--- acegen/__init__.py
+++ acegen/__init__.py
@@ -1,5 +1,4 @@
 from .rl_env.smiles_env import SMILESEnv
-from .transforms.burnin_transform import BurnInTransform
 from .transforms.penalise_repeated_transform import PenaliseRepeatedSMILES
 from .transforms.reward_transform import SMILESReward
 from .vocabulary.vocabulary import SMILESVocabulary
