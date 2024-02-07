190f0278cee02b93a64c022939d1ef5458978478
albert bou <albertbou92@gmail.com>
2024-02-07 10:32:36 +0100

    fix vocab tests

--- acegen/data/__init__.py
+++ acegen/data/__init__.py
@@ -1,2 +1,2 @@
 from acegen.data.smiles_dataset import load_dataset, SMILESDataset
-from acegen.data.utils import is_in_reference, remove_duplicates, smiles_to_tensordict
+from acegen.data.utils import smiles_to_tensordict
