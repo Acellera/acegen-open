2656ee32cf83b46e1ce6e6edc2178434486a8b7c
albert bou <albertbou92@gmail.com>
2024-02-15 11:22:17 +0100

    minor fix

--- examples/transformers_test/generate_smiles.py
+++ examples/transformers_test/generate_smiles.py
@@ -2,14 +2,11 @@ from pathlib import Path
 
 import torch
 from acegen.models import adapt_state_dict
-from acegen.models.gpt2 import GPT2
 from acegen.rl_env import generate_complete_smiles, SMILESEnv
 from acegen.vocabulary import SMILESVocabulary
 from rdkit.Chem import AllChem, QED
-from tensordict.nn import TensorDictModule
-from torchrl.envs import ExplorationType
-from torchrl.modules import ProbabilisticActor
 from transformers import GPT2Config, GPT2Model
+from acegen.models.gpt2 import create_gpt2_actor
 
 # Get available device
 device = (
@@ -41,32 +38,7 @@ env_kwargs = {
 env = SMILESEnv(**env_kwargs)
 
 # Define model
-model_config = GPT2Config()
-
-# Adjust model parameters
-model_config.vocab_size = len(vocabulary)
-model_config.n_positions = 2048
-model_config.n_head = 16
-model_config.n_layer = 24
-model_config.n_embd = 128
-model_config.attn_pdrop = 0.1
-model_config.embd_pdrop = 0.1
-model_config.resid_pdrop = 0.1
-model_config.initializer_range = 0.02
-
-policy = TensorDictModule(
-    GPT2(model_config),
-    in_keys=["sequence", "sequence_mask"],
-    out_keys=["logits"],
-)
-probabilistic_policy = ProbabilisticActor(
-    module=policy,
-    in_keys=["logits"],
-    out_keys=["action"],
-    distribution_class=torch.distributions.Categorical,
-    return_log_prob=True,
-    default_interaction_type=ExplorationType.RANDOM,
-)
+probabilistic_policy, _ = create_gpt2_actor(len(vocabulary))
 
 ckpt = torch.load(
     Path(__file__).resolve().parent.parent.parent / "priors" / "gpt2_enamine_real.ckpt"
@@ -76,11 +48,12 @@ probabilistic_policy.load_state_dict(
     adapt_state_dict(ckpt, probabilistic_policy.state_dict())
 )
 
-policy.to(device)
+probabilistic_policy.to(device)
 data = generate_complete_smiles(policy=probabilistic_policy, environment=env)
 smiles_str = [
     vocabulary.decode(smi.cpu().numpy(), ignore_indices=[0]) for smi in data["action"]
 ]
+print(smiles_str)
 
 
 def evaluate_mol(smiles: str):
