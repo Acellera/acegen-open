7a5126891e324fcab52746c38ad08b362f3aaec6
albert bou <albertbou92@gmail.com>
2024-03-11 14:59:38 +0100

    accept custom scoring functions

--- acegen/models/__init__.py
+++ acegen/models/__init__.py
@@ -23,28 +23,34 @@ models = {
         create_gru_actor,
         create_gru_critic,
         create_gru_actor_critic,
-        Path(__file__).resolve().parent.parent
+        Path(__file__).resolve().parent.parent.parent
         / "priors"
         / "chembl_filtered_vocabulary.txt",
-        Path(__file__).resolve().parent.parent / "priors" / "gru_chembl_filtered.ckpt",
+        Path(__file__).resolve().parent.parent.parent
+        / "priors"
+        / "gru_chembl_filtered.ckpt",
         SMILESTokenizer(),
     ),
     "lstm": (
         create_lstm_actor,
         create_lstm_critic,
         create_lstm_actor_critic,
-        Path(__file__).resolve().parent.parent / "priors" / "chembl_vocabulary.txt",
-        Path(__file__).resolve().parent.parent / "priors" / "lstm_chembl.ckpt",
+        Path(__file__).resolve().parent.parent.parent
+        / "priors"
+        / "chembl_vocabulary.txt",
+        Path(__file__).resolve().parent.parent.parent / "priors" / "lstm_chembl.ckpt",
         SMILESTokenizer(),
     ),
     "gpt2": (
         create_gpt2_actor,
         create_gpt2_critic,
         create_gpt2_actor_critic,
-        Path(__file__).resolve().parent.parent
+        Path(__file__).resolve().parent.parent.parent
         / "priors"
         / "enamine_real_vocabulary.txt",
-        Path(__file__).resolve().parent.parent / "priors" / "gpt2_enamine_real.ckpt",
+        Path(__file__).resolve().parent.parent.parent
+        / "priors"
+        / "gpt2_enamine_real.ckpt",
         SMILESTokenizer2(),
     ),
 }
