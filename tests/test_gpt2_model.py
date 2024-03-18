cde3b0238c7ec03f33c0e579cea2050fde1490d0
MorganCThomas <morganthomas263@gmail.com>
2024-03-18 10:49:37 +0100

    include sequence tokens in test batch

--- tests/test_gpt2_model.py
+++ tests/test_gpt2_model.py
@@ -5,6 +5,7 @@ from acegen.models.gpt2 import (
     create_gpt2_actor_critic,
     create_gpt2_critic,
 )
+from acegen.data import smiles_to_tensordict
 from tensordict import TensorDict
 from tests.utils import get_default_devices
 
@@ -13,22 +14,9 @@ def generate_valid_data_batch(
     vocabulary_size: int, batch_size: int, sequence_length: int
 ):
     tokens = torch.randint(0, vocabulary_size, (batch_size, sequence_length + 1))
-    done = torch.randint(0, 2, (batch_size, sequence_length + 1, 1), dtype=torch.bool)
-    batch = TensorDict(
-        {
-            "sequence": tokens[:, :-1],
-            "done": torch.zeros(batch_size, sequence_length, 1),
-            "is_init": done[:, 1:],
-            "next": TensorDict(
-                {
-                    "sequence": tokens[:, 1:],
-                    "done": done[:, :-1],
-                },
-                batch_size=[batch_size, sequence_length],
-            ),
-        },
-        batch_size=[batch_size, sequence_length],
-    )
+    batch = smiles_to_tensordict(tokens, replace_mask_value=0) # batch_size, sequence_length)
+    batch.set("sequence", batch.get("observation"))
+    batch.set("sequence_mask", batch.get("mask"))
     return batch
 
 
