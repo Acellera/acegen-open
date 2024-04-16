d5848ee5bb5b169c37559064002518fa0b20a66e
albert bou <albertbou92@gmail.com>
2024-04-16 17:06:31 +0200

    fix loss

--- scripts/ppo/ppo.py
+++ scripts/ppo/ppo.py
@@ -360,10 +360,8 @@ def run_ppo(cfg, task):
                 loss = loss_module(batch)
                 loss = loss.named_apply(
                     lambda name, value: (
-                        # (value * mask).mean() if name.startswith("loss_") else value
-                        (value * mask).sum(-1).mean(-1)
-                        if name.startswith("loss_")
-                        else value
+                        value * mask).mean() if name.startswith("loss_") else value
+                        # (value * mask).sum(-1).mean(-1) if name.startswith("loss_") else value
                     ),
                     batch_size=[],
                 )
