0d6d7767b5164307493279492b340c821255fd15
albert bou <albertbou92@gmail.com>
2024-08-08 18:53:02 +0200

    improve tests

--- .github/unittest/install_dependencies_nightly.sh
+++ .github/unittest/install_dependencies_nightly.sh
@@ -2,9 +2,16 @@
 python -m pip install --upgrade pip
 
 # Install dependencies
-python -m pip install flake8 pytest pytest-cov hydra-core tqdm deepsmiles selfies smi2sdf smi2svg atomInSmiles safe-mol molbloom
+python -m pip install flake8 pytest pytest-cov hydra-core tqdm
 python -m pip install torch torchvision
-python -m pip install transformers promptsmiles torchrl rdkit==2023.09.03 MolScore # causal-conv1d>=1.4.0 mamba-ssm==1.2.2
+python -m pip install transformers promptsmiles torchrl rdkit==2023.3.3 MolScore # causal-conv1d>=1.4.0 mamba-ssm==1.2.2
+python -m pip install deepsmiles
+python -m pip install selfies
+python -m pip install smi2sdf
+python -m pip install smi2svg
+python -m pip install atomInSmiles
+python -m pip install safe-mol
+python -m pip install molbloom
 
 # Verify installations
 python -c "import transformers; print(transformers.__version__)"
